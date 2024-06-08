df99f894bc46d93137b9d689fa3e08ba8e86f44a
Ankush22222 <153420643+Ankush22222@users.noreply.github.com>
2024-06-08 14:42:15 +0530

    JS Completed

---
+++ 4.Java Script Learning/Learning-1 Javascript Introduction/new.js
@@ -0,0 +1,2 @@
+console.log("code is running....")
+// java script program ko node js ki help se run kar sakte hai
---
+++ 4.Java Script Learning/Learning-1 Javascript Introduction/script.js
@@ -0,0 +1,24 @@
+alert("Hello World");
+
+
+console.log("code is running....")
+// means inspect par click kar ke console pe click karenge tab yeh shoow hoga
+
+
+
+var a = prompt("Enter your number")
+// ise website par enter your number ka pop-up aayega
+console.log("your number is " + a)
+// ise joh bhi number enter karenge voh likh kar aayega console ke ander
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+if(isTrue) {
+    console.log("computer is blasting")
+}
+
+else{
+    console.log("computer not is blasting")
+}
+
+document.title = "Hey I am good"
+// ise title change ho gaya
+document.body.style.backgroundColor="red"
